1b5e4521e6ae3da02bd818687ac0be8cc90c6bff
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-08-17 01:32:09 -0400

    Update config.tf
    s3 bucket name changed

--- vpc_prod/network/config.tf
+++ vpc_prod/network/config.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.5.0"
   backend "s3" {
-    bucket = "prodgroup37"
+    bucket = "prodgroup12"
     key    = "vpc_prod/networkfile/terraform.tfstate"
     region = "us-east-1"
   }
