793404d92afe9494d09a0d22fc8b5dfd1e3e2f7e
Shreyas Patel <61398641+371999@users.noreply.github.com>
2024-08-17 01:29:29 -0400

    Updates config.tf 2nd time

--- vpc_prod/webserver/config.tf
+++ vpc_prod/webserver/config.tf
@@ -1,6 +1,7 @@
 terraform {
+  required_version = ">= 1.5.0"
   backend "s3" {
-    bucket = "prodgroup12"
+    bucket = "prodgroup37"
     key    = "vpc_prod/webserverfile/terraform.tfstate"
     region = "us-east-1"
   }
