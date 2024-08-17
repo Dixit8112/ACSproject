c617e2922a77e42eb2ed004e95852141a72f920e
EC2 Default User <ec2-user@ip-172-31-94-174.ec2.internal>
2024-08-17 01:46:40 +0000

    added the code to output.tf file

---
+++ webserver/output.tf
@@ -0,0 +1,28 @@
+output "bastion_publicIP" {
+  value = aws_instance.bastionhost.public_ip
+}
+
+output "bastion_privateIP" {
+  value = aws_instance.bastionhost.private_ip
+}
+
+output "privateVM5_ip" {
+  value = aws_instance.VM5.private_ip
+}
+
+output "privateVM6_ip" {
+  value = aws_instance.VM6.private_ip
+}
+
+output "publicVM1_ip" {
+  value = aws_instance.publicVM1.public_ip
+}
+
+output "publicVM3_ip" {
+  value = aws_instance.publicVM3.public_ip
+}
+
+output "publicVM4_ip" {
+  value = aws_instance.publicVM4.public_ip
+}
+
